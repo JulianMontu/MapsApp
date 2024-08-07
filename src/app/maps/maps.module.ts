7cde325a37c4c11c823bb1f2c8a46d28c1707524
JulianMontu <julamontufar@misena.edu.co>
2024-08-07 11:56:00 -0500

    Standalone components como pagina y como componente

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
@@ -12,6 +11,7 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
 
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoianVsaWFubW9udHVmYXIiLCJhIjoiY2x6NjF6cXEyMmN1bTJqcHFranY0MDc3cCJ9.OSGycipLaJT8XzXHkJBqRA';
 
@@ -20,7 +20,6 @@ mapboxgl.accessToken = 'pk.eyJ1IjoianVsaWFubW9udHVmYXIiLCJhIjoiY2x6NjF6cXEyMmN1b
 @NgModule({
   declarations: [
     MiniMapComponent,
-    SideMenuComponent,
     MapsLayoutComponent,
     FullScreenPageComponent,
     MarkersPageComponent,
@@ -30,7 +29,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoianVsaWFubW9udHVmYXIiLCJhIjoiY2x6NjF6cXEyMmN1b
   imports: [
     CommonModule,
     MapsRoutingModule,
-    CounterAloneComponent
+    CounterAloneComponent,
+    SideMenuComponent,
   ]
 })
 export class MapsModule { }
